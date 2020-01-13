d7d7a3e1413eb3f51f4ce68a3f2b51d515457ab9
Debasis Ganguly <dganguly@debasiss-mbp-2.mul.ie.ibm.com>
2020-01-13 14:43:55 +0000

    tfidf benchmark added

---
+++ src/main/java/org/rationalarithmetic/Rational.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.rationalarithmetic;
+
+import java.util.Comparator;
+import java.util.Random;
+
+/**
+ *
+ * @author dganguly
+ */
+public class Rational {
+    int num;
+    int denom;
+    
+    public static Rational opResult = new Rational();
+    public static float opResult_f;
+    
+    static final int SEED = 123456;
+    static Random randomizer = new Random(SEED);
+
+    public Rational() { }
+    
+    public Rational(int num, int denom) {
+        this.num = num;
+        this.denom = denom;
+    }
+    
+    public void add(Rational that) {
+        opResult.num = this.num*that.denom + that.num*this.denom;
+        opResult.denom = that.denom*this.denom;
+    }
+    
+    public void subtract(Rational that) {
+        opResult.num = this.num*that.denom - that.num*this.denom;
+        opResult.denom = that.denom*this.denom;
+    }
+    
+    public void mediant(Rational that) {
+        opResult.num = this.num + that.num;
+        opResult.denom = this.denom + that.denom;
+    }
+    
+    public void mul(Rational that) {
+        opResult.num = this.num*that.num;
+        opResult.denom = this.denom*that.denom;
+    }
+    
+    public void div(Rational that) {
+        opResult.num = this.num*that.denom;
+        opResult.denom = this.denom*that.num;
+    }
+    
+    public float getValue() { return num/(float)denom; }
+    
+    @Override
+    public String toString() { return num + "/" + denom; }
+    
+    public static Rational initRandom(int maxNumerator, int maxDenominator) {
+        int num = 1 + randomizer.nextInt(maxNumerator);
+        int denom = 1 + randomizer.nextInt(maxDenominator);
+        return new Rational(num, denom);
+    }    
+}
